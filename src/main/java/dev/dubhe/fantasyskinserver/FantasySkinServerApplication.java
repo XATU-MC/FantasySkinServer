171afc37e852bb6ae51a40699f4732dc37fa5b03
Gugle <administration@ztxy666.cn>
2024-02-26 00:23:48 +0800

    init

---
+++ src/main/java/dev/dubhe/fantasyskinserver/FantasySkinServerApplication.java
@@ -0,0 +1,13 @@
+package dev.dubhe.fantasyskinserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasySkinServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FantasySkinServerApplication.class, args);
+    }
+
+}
